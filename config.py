1ab19275da0cb5cfa06aed4ac1502ce47b20ece5
ombaejr <ombaejr@gmail.com>
2019-06-26 04:11:23 +0300

    Include configurations for my app

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+class Config:
+    '''
+    General config class
+    '''
+
+    ROVER_API_SOURCES_URL ='https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1337&api_key={}'
+
+
+    ROVER_API_KEY = os.environ.get('ROVER_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}  
