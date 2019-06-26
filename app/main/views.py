892813ab0abf5d19d5c1feab194cccdfe667429a
ombaejr <ombaejr@gmail.com>
2019-06-26 04:33:15 +0300

    Write function that renders the index page and its data

---
+++ app/main/views.py
@@ -0,0 +1,16 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_photos
+
+# Views
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    #getting photos
+    photos = get_photos()
+    print('photos')
+
+    return render_template('index.html',photos = photos)
